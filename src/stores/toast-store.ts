bfe72a978a311d68e74d625c606b33916ebfd334
johnivanpuayap <johnivanpuayap@gmail.com>
2026-04-16 13:07:48 +0800

    add toast store with zustand

---
+++ src/stores/toast-store.ts
@@ -0,0 +1,26 @@
+import { create } from "zustand";
+
+interface Toast {
+  id: string;
+  type: "success" | "error";
+  message: string;
+  onRetry?: () => void;
+}
+
+interface ToastStore {
+  toasts: Toast[];
+  addToast: (toast: Omit<Toast, "id">) => void;
+  removeToast: (id: string) => void;
+}
+
+export const useToastStore = create<ToastStore>((set) => ({
+  toasts: [],
+  addToast: (toast) =>
+    set((state) => ({
+      toasts: [...state.toasts, { ...toast, id: crypto.randomUUID() }],
+    })),
+  removeToast: (id) =>
+    set((state) => ({
+      toasts: state.toasts.filter((t) => t.id !== id),
+    })),
+}));
